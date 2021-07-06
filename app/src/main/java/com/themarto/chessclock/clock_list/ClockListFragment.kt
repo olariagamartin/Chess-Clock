68530fb13d80caacc814ec24d261e224e27d9455
Olariaga Martin <olariagamartin@gmail.com>
2021-07-05 22:31:57 -0300

    Capture click on add clock fab

--- app/src/main/java/com/themarto/chessclock/clock_list/ClockListFragment.kt
+++ app/src/main/java/com/themarto/chessclock/clock_list/ClockListFragment.kt
@@ -5,9 +5,8 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.themarto.chessclock.R
+import android.widget.Toast
 import com.themarto.chessclock.databinding.FragmentClockListBinding
-import com.themarto.chessclock.databinding.FragmentClocksBinding
 
 class ClockListFragment : Fragment() {
 
@@ -19,6 +18,10 @@ class ClockListFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         _binding = FragmentClockListBinding.inflate(inflater, container, false)
+
+        binding.addClock.setOnClickListener{
+            Toast.makeText(context, "Add clock", Toast.LENGTH_SHORT).show()
+        }
         return binding.root
     }
 
