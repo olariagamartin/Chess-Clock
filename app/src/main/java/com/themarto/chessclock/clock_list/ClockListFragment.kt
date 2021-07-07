ad46800633480a9197c2edba2963ab1b2bc2d118
Olariaga Martin <olariagamartin@gmail.com>
2021-07-07 20:45:03 -0300

    Adapter working, kapt plugin added (needed for room)

--- app/src/main/java/com/themarto/chessclock/clock_list/ClockListFragment.kt
+++ app/src/main/java/com/themarto/chessclock/clock_list/ClockListFragment.kt
@@ -6,18 +6,28 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
+import androidx.lifecycle.ViewModelProvider
 import com.themarto.chessclock.databinding.FragmentClockListBinding
 
 class ClockListFragment : Fragment() {
 
     private var _binding: FragmentClockListBinding? = null
     private val binding get() = _binding!!
+    private lateinit var viewModel: ClockListViewModel
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         _binding = FragmentClockListBinding.inflate(inflater, container, false)
+        val application = requireActivity().application
+        val viewModelFactory = ClockListViewModelFactory(application)
+        viewModel = ViewModelProvider(this, viewModelFactory).get(ClockListViewModel::class.java)
+
+        val adapter = ClockListAdapter()
+        adapter.data = viewModel.getDummyData()
+
+        binding.clockList.adapter = adapter
 
         binding.addClock.setOnClickListener{
             Toast.makeText(context, "Add clock", Toast.LENGTH_SHORT).show()
--- app/src/main/java/com/themarto/chessclock/clock_list/ClockListViewModel.kt
+++ app/src/main/java/com/themarto/chessclock/clock_list/ClockListViewModel.kt
@@ -2,8 +2,26 @@ package com.themarto.chessclock.clock_list
 
 import android.app.Application
 import androidx.lifecycle.ViewModel
+import com.themarto.chessclock.database.ChessClock
 import com.themarto.chessclock.database.ChessClockDatabase
+import com.themarto.chessclock.utils.ChessUtils.Companion.BLITZ_THUMBNAIL
+import com.themarto.chessclock.utils.ChessUtils.Companion.BULLET_THUMBNAIL
+import com.themarto.chessclock.utils.ChessUtils.Companion.CLASSIC_THUMBNAIL
+import com.themarto.chessclock.utils.ChessUtils.Companion.RAPID_THUMBNAIL
+import com.themarto.chessclock.utils.MyCountDownTimer.Companion.ONE_MINUTE
 
 class ClockListViewModel(application: Application): ViewModel() {
     private val database = ChessClockDatabase.getInstance(application).chessClockDao
+
+    fun getDummyData(): List<ChessClock>{
+        return arrayListOf(
+            ChessClock(0, ONE_MINUTE, ONE_MINUTE, BULLET_THUMBNAIL, "Bullet"),
+            ChessClock(1, 2*ONE_MINUTE, 2*ONE_MINUTE, BULLET_THUMBNAIL, "Bullet"),
+            ChessClock(2, 5*ONE_MINUTE, 5*ONE_MINUTE, BLITZ_THUMBNAIL, "Blitz"),
+            ChessClock(3, 10*ONE_MINUTE, 10*ONE_MINUTE, RAPID_THUMBNAIL, "Rapid"),
+            ChessClock(4, 15*ONE_MINUTE, 15*ONE_MINUTE, RAPID_THUMBNAIL, "Rapid"),
+            ChessClock(5, 60*ONE_MINUTE, 60*ONE_MINUTE, CLASSIC_THUMBNAIL, "Classic"),
+
+        )
+    }
 }
