284d0a2052a36086314b051774bfbc73f3ad78a0
Olariaga Martin <olariagamartin@gmail.com>
2021-07-08 23:13:56 -0300

    Game type set, getDefaultData method set, game type name added to string resources

--- app/src/main/java/com/themarto/chessclock/utils/ChessUtils.kt
+++ app/src/main/java/com/themarto/chessclock/utils/ChessUtils.kt
@@ -1,10 +1,30 @@
 package com.themarto.chessclock.utils
 
+import android.content.res.Resources
+import com.themarto.chessclock.database.ChessClock
+import com.themarto.chessclock.utils.MyCountDownTimer.Companion.ONE_MINUTE
+
 class ChessUtils {
     companion object {
-        const val BULLET_THUMBNAIL = 5
-        const val BLITZ_THUMBNAIL = 10
-        const val RAPID_THUMBNAIL = 60
-        const val CLASSIC_THUMBNAIL = 100
+        const val BULLET = 5
+        const val BLITZ = 10
+        const val RAPID = 60
+        const val CLASSIC = 100
+    }
+}
+
+class DataUtil {
+    companion object {
+        fun getDefaultClocks (resources: Resources): List<ChessClock> {
+            return arrayListOf(
+                ChessClock(firstPlayerTime = ONE_MINUTE, secondPlayerTime = ONE_MINUTE),
+                ChessClock(firstPlayerTime = 2*ONE_MINUTE, secondPlayerTime = 2*ONE_MINUTE),
+                ChessClock(firstPlayerTime = 5*ONE_MINUTE, secondPlayerTime = 5*ONE_MINUTE),
+                ChessClock(firstPlayerTime = 10*ONE_MINUTE, secondPlayerTime = 10*ONE_MINUTE),
+                ChessClock(firstPlayerTime = 15*ONE_MINUTE, secondPlayerTime = 15*ONE_MINUTE),
+                ChessClock(firstPlayerTime = 60*ONE_MINUTE, secondPlayerTime = 60*ONE_MINUTE)
+            )
+        }
+
     }
 }
