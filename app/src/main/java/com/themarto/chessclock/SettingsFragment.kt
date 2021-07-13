eb1518a365dd9c5558c709fd18730cf47a9f3bcd
Olariaga Martin <olariagamartin@gmail.com>
2021-07-13 15:59:11 -0300

    Connect actions with shared preferences

--- app/src/main/java/com/themarto/chessclock/SettingsFragment.kt
+++ app/src/main/java/com/themarto/chessclock/SettingsFragment.kt
@@ -1,10 +1,14 @@
 package com.themarto.chessclock
 
+import android.content.Context
+import android.content.SharedPreferences
 import android.os.Bundle
+import android.text.format.DateUtils
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.CompoundButton
 import androidx.navigation.fragment.findNavController
 import com.themarto.chessclock.databinding.FragmentSettingsBinding
 
@@ -12,6 +16,14 @@ class SettingsFragment : Fragment() {
 
     private var _binding: FragmentSettingsBinding? = null
     private val binding: FragmentSettingsBinding get() = _binding!!
+    private lateinit var pref: SharedPreferences
+
+    companion object {
+        const val SOUND_AFTER_MOVE_KEY = "sound_after_move"
+        const val VIBRATE_KEY = "vibrate"
+        const val LOW_TIME_WARNING_KEY = "low_time_warning"
+        const val ALERT_TIME_KEY = "alert_time"
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -19,13 +31,46 @@ class SettingsFragment : Fragment() {
     ): View? {
         _binding = FragmentSettingsBinding.inflate(inflater, container, false)
 
+        pref = requireActivity().getPreferences(Context.MODE_PRIVATE)
+
+        loadData()
+
         // UI ACTIONS
         binding.toolbar.setNavigationOnClickListener {
             findNavController().navigateUp()
         }
+
+        binding.soundSwitch.setOnCheckedChangeListener{ _, isChecked ->
+            pref.edit().putBoolean(SOUND_AFTER_MOVE_KEY, isChecked).apply()
+        }
+
+        binding.vibrateSwitch.setOnCheckedChangeListener{ _, isChecked ->
+            pref.edit().putBoolean(VIBRATE_KEY, isChecked).apply()
+        }
+
+        binding.lowTimeWarningSwitch.setOnCheckedChangeListener{ _, isChecked ->
+            pref.edit().putBoolean(LOW_TIME_WARNING_KEY, isChecked).apply()
+        }
+
+        // todo: add time selector to save on pref
         //...
 
         return binding.root
+    }
+
+    private fun loadData() {
+        val soundAfterMove = pref.getBoolean(SOUND_AFTER_MOVE_KEY, true)
+        binding.soundSwitch.isChecked = soundAfterMove
+
+        val vibrate = pref.getBoolean(VIBRATE_KEY, true)
+        binding.vibrateSwitch.isChecked = vibrate
+
+        val lowTimeWarning = pref.getBoolean(LOW_TIME_WARNING_KEY, false)
+        binding.lowTimeWarningSwitch.isChecked = lowTimeWarning
+
+        val alertTime = pref.getLong(ALERT_TIME_KEY, 0)
+        val alertTimeText = DateUtils.formatElapsedTime(alertTime)
+        binding.alertTimeSummary.text = alertTimeText
     }
 
     override fun onDestroy() {
